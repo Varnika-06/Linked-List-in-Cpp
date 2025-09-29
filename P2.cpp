5635d2a695bb89cb1a03cceaa14e4944909d7691
Varnika-06 <varnikastuff234@gmail.com>
2025-09-29 20:28:06 +0530

    Create P2.cpp

---
+++ P2.cpp
@@ -0,0 +1,51 @@
+//Name: Varnika Maurya
+//PRN: 24070123160
+//Exp No.: 17
+//Program No.: 2
+#include <iostream>
+using namespace std;
+class Node 
+{
+    public:
+        int value;
+        Node* next;
+        Node(int val) 
+        {
+            value = val;
+            next = nullptr;
+        }
+};
+void addToFront(Node*& head, int val) 
+{
+    Node* newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+}
+void printList(Node* head) 
+{
+    Node* current = head;
+    while (current != nullptr) 
+    {
+        cout << current->value << " -> ";
+        current = current->next;
+    }
+    cout << "NULL" << endl;
+}
+int main() 
+{
+    Node* head = nullptr;
+    addToFront(head, 100);
+    printList(head);
+    addToFront(head, 200);
+    printList(head);
+    addToFront(head, 300);
+    printList(head);
+    return 0;
+}
+
+/*
+Output:
+100 -> NULL
+200 -> 100 -> NULL
+300 -> 200 -> 100 -> NULL
+*/
